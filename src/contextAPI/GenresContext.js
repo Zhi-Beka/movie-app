24767a9b616600d1a549d58fb09289534fb8907d
bektemirova <jibek260886@gmail.com>
2023-02-07 21:06:19 +0300

    change provider

--- src/contextAPI/GenresContext.js
+++ src/contextAPI/GenresContext.js
@@ -1,8 +1,3 @@
-//import React from 'react';
-
-//const { Provider: GenresProvider, Consumer: GenresConsumer } = React.createContext();
-
-//export { GenresProvider, GenresConsumer };
 // eslint-disable-next-line prettier/prettier
 import {createContext} from 'react';
 
